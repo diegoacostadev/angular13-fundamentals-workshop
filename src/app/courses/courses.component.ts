2cb935366b76a3400f20cd625534103eb7d2519d
Diego Acosta <diegoacosta211@gmail.com>
2023-05-13 13:32:11 -0300

    render courses

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,12 +13,36 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Javascript: The Hard parts v2',
+      description: 'Learn javascript like a pro!',
+      percentComplete: 34,
+      favorite: true
+    },
+    {
+      id: 3,
+      title: 'NextJs Introduction',
+      description: 'Learn the fundamentals of NextJs with Sott',
+      percentComplete: 10,
+      favorite: true
     }
   ];
+
+  selectedCourse = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(id: number) {
+    this.courses = this.courses.filter(c => c.id !== id);
   }
 
 }
